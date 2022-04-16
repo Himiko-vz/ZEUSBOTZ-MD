ebb26ed1f8de4b9c00b4ed048c110cfe07cd87e3
Himiko-vz <103635626+Himiko-vz@users.noreply.github.com>
2022-04-16 13:17:46 +0700

    Update galau.js

--- plugins/galau.js
+++ plugins/galau.js
@@ -20,7 +20,7 @@ ${pickRandom(global.galau)}
            hydratedButtons: [{
              urlButton: {
                displayText: 'MY INSTAGRAM 💌',
-               url: 'https://instagram.com/en4y.pin'
+               url: 'https://instagram.com/Kevinafsar_'
              }
 
            },
