ea42a560c1e7da628706d55686cf717c7f821e59
Himiko-vz <103635626+Himiko-vz@users.noreply.github.com>
2022-04-17 07:34:39 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,9 +3,9 @@ const PhoneNumber = require('awesome-phonenumber')
 let ftroli = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "6289523258649-1604595598@g.us"}, "message": {orderMessage: {itemCount: 1,status: 200, surface: 200, message: `                   「 MY OWNER 」            `, orderTitle: 'Centauri', sellerJid: '0@s.whatsapp.net'}}}
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:; Alpin;;;'
-                    + 'FN: Alpin\n' // full name
-                    + 'ORG:Alpin;\n' // the organization of the contact
+                    + 'N:; Kevin;;;'
+                    + 'FN: Kevin\n' // full name
+                    + 'ORG:Kevin;\n' // the organization of the contact
                     + 'TEL;type=CELL;type=VOICE;waid=6283131214121:+62 831-3121-4121\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'kevin', contacts: [{ vcard }] } }, { quoted: ftroli })
